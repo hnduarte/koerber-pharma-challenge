85035717e7a77ebe4f4cba353ef03a3be9f5c232
hbonifac <henrique.bonifAcioduarte@capgemini.com>
2024-10-31 16:05:07 +0000

    Initial Commit

---
+++ src/main/java/com/koerber/pharma/koerber_pharma_challenge/KoerberPharmaBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.koerber.pharma.koerber_pharma_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoerberPharmaBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoerberPharmaBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koerber/pharma/koerber_pharma_challenge/KoerberPharmaBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koerber.pharma.koerber_pharma_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoerberPharmaBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
